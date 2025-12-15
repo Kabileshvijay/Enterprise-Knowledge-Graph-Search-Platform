021bb5df0adeb4ef87fe909ebf7d91538c5902a7
KABILESHVIJAY P R <153415977+Kabileshvijay@users.noreply.github.com>
2025-12-15 22:49:43 +0530

    backend setup

---
+++ backend/src/test/java/com/enterprise/knowledge/KnowledgePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
