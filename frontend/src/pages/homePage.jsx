66a3f905581c4b3a692f0ea3992f63ecf650514a
KABILESHVIJAY P R <153415977+Kabileshvijay@users.noreply.github.com>
2025-12-16 15:55:40 +0530

    added homePage

---
+++ frontend/src/pages/homePage.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Welcome to EntroGraph</h1>
+      <p>This is the Home Page</p>
+    </div>
+  );
+};
+
+export default Home;
